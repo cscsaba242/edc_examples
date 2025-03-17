ef84b7772262166f0b997628032c75190e4fc6fc
cscsaba242 <cscsaba242@live.com>
2025-03-17 08:04:02 -0900

    policy

---
+++ policy/policy-01-policy-enforcement/policy-functions/build/docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.eclipse.edc.sample.extension.policy","l":"LocationConstraintFunction"},{"p":"org.eclipse.edc.sample.extension.policy","l":"PolicyFunctionsExtension"}];updateSearchResults();
